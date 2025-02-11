9a519fd362a07eeb3505775d05fa07aa5a84c5fe
utkarsh51103 <utkarshsharma210900@acropolis.in>
2025-02-11 11:18:36 +0530

    marker

--- client/src/Pages/Map/components/MapArea/index.jsx
+++ client/src/Pages/Map/components/MapArea/index.jsx
@@ -24,13 +24,14 @@ function MapArea() {
   const routenumber = useSelector((state)=>state.routenumber.number);
 
   const markerIcon = new L.Icon({
-    iconUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-icon.png", 
-    iconSize: [25, 41],  
-    iconAnchor: [12, 41], 
-    popupAnchor: [1, -34], 
-    shadowUrl: "https://unpkg.com/leaflet@1.9.4/dist/images/marker-shadow.png", 
-    shadowSize: [41, 41]
+    iconUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png",
+    shadowUrl: "https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png",
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41],
 });
+
 
 
   useEffect(() => {
